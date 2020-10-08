8db3622c71d2cb90b3b28f19ebf5747e139621e6
capable-optimization-2034 <brianna.l.castor@gmail.com>
2020-10-08 00:35:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -39,4 +39,14 @@ function doToEvens(callback) {
 function doToElementsInArray(array, callback) {
   array.forEach(callback);
 }
+function changeCompletely(element, index, array) {
+  array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
+}
+ 
+var animals = ["dog", "cat", "squirrel"];
+ 
+doToElementsInArray(animals, changeCompletely);
+ 
+// log out animals -- pretty cool, right?
+console.log(animals);
 // Add your changeCompletely() function here:
